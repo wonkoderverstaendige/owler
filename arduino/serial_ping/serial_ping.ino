e0820b145e9e5126f1ad00b418fc367d63cfc2f7
Ronny Eichler <R.Eichler@science.ru.nl>
2018-01-18 12:49:12 +0100

    Minimal ping benchmarking

--- arduino/serial_ping/serial_ping.ino
+++ arduino/serial_ping/serial_ping.ino
@@ -3,12 +3,14 @@
 
 const int pin_LED = 13;
 volatile char inChar;
+unsigned long ts_start = micros();
+unsigned long ts_end = micros();
 
 void setup() {
   pinMode(pin_LED, OUTPUT);
   digitalWrite(pin_LED, LOW);
   
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -17,10 +19,29 @@ void loop() {
 void serialEvent() {
   while (Serial.available()) {
     inChar = (char)Serial.read();
-    if (inChar == '1') {
-      digitalWriteFast(pin_LED, HIGH);
-    } else {
-      digitalWriteFast(pin_LED, LOW);
+    switch (inChar) {
+      case '1':
+        digitalWriteFast(pin_LED, HIGH);
+        break;
+        
+      case '0':
+        digitalWriteFast(pin_LED, LOW);
+        break;
+        
+      case 'R':
+        ts_end = micros();
+        Serial.println(ts_end - ts_start);
+        break;
+        
+      case 'S':
+        // start loop
+        Serial.println('!');
+        Serial.send_now();
+        ts_start = micros();
+        break;
+        
+      default:
+        break;     
     }
   }
 }
